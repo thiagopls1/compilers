1535ec210bc6c3ca4102c7d6189f1dc692870cbc
thiagopls1 <thiagopls1@hotmail.com>
2026-03-16 22:04:57 -0300

    feat: add operator handling

--- ex_aula5/main.c
+++ ex_aula5/main.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef enum {
@@ -18,6 +19,20 @@ bool isoperator(char character) {
   return (char *)strchr(operators, character) != NULL;
 }
 
+void handleDigit() {}
+
+void handleOperator(char *str_pointer, Token *tokens, int tokens_count) {
+  char *token_value = (char *)malloc(sizeof(char));
+  *token_value = *str_pointer;
+  Token new_token = {
+      .type = OP,
+  };
+  new_token.value = token_value;
+
+  tokens[tokens_count] = new_token;
+  str_pointer++;
+}
+
 int main(int argc, char *argv[]) {
   if (argc <= 1) {
     printf("Erro: necessário passar parâmetro!\n");
@@ -25,16 +40,26 @@ int main(int argc, char *argv[]) {
   }
 
   char *str_pointer = argv[1];
+  Token tokens[strlen(str_pointer)];
 
-  // Iterate until end of string
+  int tokens_count = 0;
   while (*str_pointer != '\0') {
-    if (isdigit(*str_pointer))
+    if (isdigit(*str_pointer)) {
       printf("Dígito detectado!\n");
+      // tokens_count++;
+    }
 
-    if (isoperator(*str_pointer))
+    if (isoperator(*str_pointer)) {
       printf("Operador detectado!\n");
+      handleOperator(str_pointer, tokens, tokens_count);
+      tokens_count++;
+    }
 
     str_pointer++;
+  }
+
+  for (int i = 0; i < tokens_count; i++) {
+    printf("%s\n", tokens[i].value);
   }
 
   printf("\n");
