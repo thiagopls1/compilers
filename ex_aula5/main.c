ca7b4455248dc75531904a21e488b7530173cf38
thiagopls1 <thiagopls1@hotmail.com>
2026-03-17 11:51:33 -0300

    fix: null terminator issues

--- ex_aula5/main.c
+++ ex_aula5/main.c
@@ -14,36 +14,34 @@ typedef struct {
 } Token;
 
 bool isoperator(char character) {
-  char *operators = "+-*/";
-  return (char *)strchr(operators, character) != NULL;
+  return (char *)strchr("+-*/", character) != NULL;
 }
 
 bool isnumber(char character) {
-  char *numbers = "1234567890";
-  return (char *)strchr(numbers, character) != NULL;
+  return (char *)strchr("1234567890", character) != NULL;
 }
 
 void handleDigit(char **str_pointer, Token *tokens, int tokens_count) {
   char *start_pointer = *str_pointer;
-  char *current_pointer = *str_pointer;
   int token_length = 0;
-  while (isnumber(*current_pointer) && *current_pointer != '\0') {
-    current_pointer = *str_pointer;
+
+  while (isnumber(**str_pointer) && **str_pointer != '\0') {
+    (*str_pointer)++;
     token_length++;
-    current_pointer++;
-    *str_pointer = current_pointer;
   }
 
-  char *token_value = malloc(token_length * sizeof(char));
+  char *token_value = malloc(token_length + 1);
 
   for (int i = 0; i < token_length; i++) {
     token_value[i] = *start_pointer;
     start_pointer++;
   }
+  token_value[token_length] = '\0';
 
-  Token new_token = {.type = NUM};
-  new_token.value = token_value;
-  tokens[tokens_count] = new_token;
+  tokens[tokens_count] = (Token){
+      .type = NUM,
+      .value = token_value,
+  };
 }
 
 void handleOperator(char *str_pointer, Token *tokens, int tokens_count) {
@@ -63,6 +61,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  int exit_code = 0;
   char *str_pointer = argv[1];
   Token *tokens = malloc(strlen(argv[1]) * sizeof(Token));
 
@@ -71,23 +70,40 @@ int main(int argc, char *argv[]) {
     if (isnumber(*str_pointer)) {
       handleDigit(&str_pointer, tokens, tokens_count);
       tokens_count++;
+      continue;
     }
 
     if (isoperator(*str_pointer)) {
       handleOperator(str_pointer, tokens, tokens_count);
       tokens_count++;
+      str_pointer++;
+      continue;
     }
 
-    str_pointer++;
+    // Se não passou em nenhuma condição, então temos um token inesperado!
+    break;
+  }
+  printf("str_pointer finalizou com: %c\n", *str_pointer);
+
+  if (*str_pointer != '\0') {
+    printf("Erro: token inesperado '%c'\n", *str_pointer);
+    exit_code = 1;
+  } else {
+    for (int i = 0; i < tokens_count; i++) {
+      // Se chegou no NULL, significa que acabou
+      if (&tokens[i] == NULL) {
+        break;
+      }
+      printf("Token %d: TIPO: %d, VALOR: %s\n", i, tokens[i].type,
+             tokens[i].value);
+    }
   }
 
   for (int i = 0; i < tokens_count; i++) {
-    // Se chegou no NULL, significa que acabou
-    if (&tokens[i] == NULL) {
-      break;
-    }
-    printf("Token %d: TIPO: %d, VALOR: %s\n", i, tokens[i].type,
-           tokens[i].value);
+    free(tokens[i].value);
   }
-  return 0;
+
+  free(tokens);
+
+  return exit_code;
 }
